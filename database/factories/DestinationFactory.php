a3f512aa14837b3a1806ea917aab34293f3f4951
Dach0 <damjan.krivacevic@gmail.com>
2019-06-25 13:57:56 +0200

    destination table added and seeded

---
+++ database/factories/DestinationFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Destination;
+use Faker\Generator as Faker;
+
+$factory->define(Destination::class, function (Faker $faker) {
+    return [
+        'destination_name' =>  $faker->randomElement($array = array ('Tianjin', 'Bari', 'Rotterdam', 'Port Klang','Hamburg'), $count = 1, $allowDuplicates = false), 
+        'distance' => $faker->numberBetween($min = 80, $max = 1200) 
+    ];
+});     
